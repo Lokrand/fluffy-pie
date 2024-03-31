9d7205fdef62ece2e2d8a2995f23a99f18054d2b
Lokrand <Uchiha132132qqq@gmail.com>
2024-03-31 20:51:31 +0300

    change index.html head

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
@@ -16,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React123
         </a>
       </header>
     </div>
